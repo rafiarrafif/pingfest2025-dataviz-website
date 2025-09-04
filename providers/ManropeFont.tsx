9882e8c102a4f6027eedf78f94efaf51c3268542
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-04 23:58:40 +0700

    :bug: (fix) changing fonts directory

--- providers/ManropeFont.tsx
+++ providers/ManropeFont.tsx
@@ -4,37 +4,37 @@ import localFont from "next/font/local";
 const Manrope = localFont({
   src: [
     {
-      path: "../fonts/Manrope-ExtraBold.ttf",
+      path: "../fonts/Manrope/Manrope-ExtraBold.ttf",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../fonts/Manrope-Bold.ttf",
+      path: "../fonts/Manrope/Manrope-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../fonts/Manrope-SemiBold.ttf",
+      path: "../fonts/Manrope/Manrope-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../fonts/Manrope-Medium.ttf",
+      path: "../fonts/Manrope/Manrope-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../fonts/Manrope-Regular.ttf",
+      path: "../fonts/Manrope/Manrope-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/Manrope-Light.ttf",
+      path: "../fonts/Manrope/Manrope-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../fonts/Manrope-ExtraLight.ttf",
+      path: "../fonts/Manrope/Manrope-ExtraLight.ttf",
       weight: "200",
       style: "normal",
     },
