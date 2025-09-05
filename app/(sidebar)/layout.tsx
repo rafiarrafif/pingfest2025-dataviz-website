71dcf72004a26ed67d3affac54f090ff48494cc6
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-05 22:27:26 +0700

    :iphone: (layout) create block for page components

--- app/(sidebar)/layout.tsx
+++ app/(sidebar)/layout.tsx
@@ -3,14 +3,16 @@ import React from "react";
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div className="flex h-screen w-screen p-2">
+    <div className="flex gap-2 h-screen w-screen p-2">
       <div className="bg-[#f7f7f7] border border-[#f1f1f1] h-full w-[280px] shadow-sm rounded-xl">
         <h1 className="font-bold text-2xl text-center mt-8 tracking-tight">
           Kopixel
         </h1>
         <SidebarNavigation />
       </div>
-      <div>{children}</div>
+      <div className="bg-[#f7f7f7] border border-[#f1f1f1] h-full flex-1 shadow-sm rounded-xl">
+        {children}
+      </div>
     </div>
   );
 };
