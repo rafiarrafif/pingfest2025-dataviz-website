03ad3bd214ab247899b46eb5c88dab371bd7dc9e
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-05 21:46:40 +0700

    :wrench: (config) edit color scheme

--- app/hero.ts
+++ app/hero.ts
@@ -1,2 +1,35 @@
 import { heroui } from "@heroui/react";
-export default heroui();
+export default heroui({
+  themes: {
+    light: {
+      colors: {
+        primary: {
+          50: "#f7ffeb",
+          100: "#e8fec9",
+          200: "#d0fd99",
+          300: "#b1f85e",
+          400: "#93ee2d",
+          500: "#73d40e",
+          600: "#56aa06",
+          700: "#43810a",
+          800: "#2f5611",
+          900: "#163003",
+          DEFAULT: "#56aa06",
+        },
+        secondary: {
+          50: "#f6f6f6",
+          100: "#e7e7e7",
+          200: "#d1d1d1",
+          300: "#b0b0b0",
+          400: "#888888",
+          500: "#6d6d6d",
+          600: "#5d5d5d",
+          700: "#4f4f4f",
+          800: "#454545",
+          900: "#000000",
+          DEFAULT: "#888888",
+        },
+      },
+    },
+  },
+});
