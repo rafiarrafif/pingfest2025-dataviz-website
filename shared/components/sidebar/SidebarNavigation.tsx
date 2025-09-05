23388a266261fb32a9c38e593641cca107b888dd
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-05 20:11:32 +0700

    :children_crossing: (ux) disable button on the current page

--- shared/components/sidebar/SidebarNavigation.tsx
+++ shared/components/sidebar/SidebarNavigation.tsx
@@ -12,10 +12,11 @@ const SidebarNavigation = () => {
         color={route === "/" ? "primary" : "secondary"}
         radius="none"
         variant="light"
-        className="font-semibold text-md justify-start bg-transparent data-[hover=true]:bg-transparent"
+        className="font-semibold text-md justify-start bg-transparent data-[hover=true]:bg-transparent opacity-100"
         as={Link}
         href="/"
         startContent={<Icon icon="solar:widget-4-bold" className="w-5 h-5" />}
+        isDisabled={route === "/"}
       >
         Dashboard
       </Button>
@@ -23,12 +24,13 @@ const SidebarNavigation = () => {
         color={route === "/tutorial" ? "primary" : "secondary"}
         radius="none"
         variant="light"
-        className="font-semibold text-md justify-start bg-transparent data-[hover=true]:bg-transparent"
+        className="font-semibold text-md justify-start bg-transparent data-[hover=true]:bg-transparent opacity-100"
         as={Link}
         href="/tutorial"
         startContent={
           <Icon icon="solar:lightbulb-minimalistic-bold" className="w-5 h-5" />
         }
+        isDisabled={route === "/tutorial"}
       >
         Tutorial
       </Button>
@@ -36,12 +38,13 @@ const SidebarNavigation = () => {
         color={route === "/about-us" ? "primary" : "secondary"}
         radius="none"
         variant="light"
-        className="font-semibold text-md justify-start bg-transparent data-[hover=true]:bg-transparent"
+        className="font-semibold text-md justify-start bg-transparent data-[hover=true]:bg-transparent opacity-100"
         as={Link}
         href="/about-us"
         startContent={
           <Icon icon="solar:question-square-bold" className="w-5 h-5" />
         }
+        isDisabled={route === "/about-us"}
       >
         About Us
       </Button>
